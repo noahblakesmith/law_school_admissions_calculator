de45e2b93d6009fe5a51afe20c32ca1f427df05e
SMHuTH8VsL <nbs56@georgetown.edu>
2022-12-09 18:57:47 -0500

    Configured Heroku further

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(routes)
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
